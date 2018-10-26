322fd3655b823bb06045b47e8d9cf1d7f2a1be3d
coasterka <tsvetanovah@gmail.com>
2018-10-26 17:55:21 +0300

    Initial code setup

---
+++ QuickPay/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuickPay.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private ILoggerManager _logger;
+
+        public ValuesController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Info message");
+            _logger.LogDebug("Debug message");
+            _logger.LogWarn("Warn message");
+            _logger.LogError("Error message");
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
